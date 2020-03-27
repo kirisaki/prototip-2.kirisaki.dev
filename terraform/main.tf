f76475f40ae9d6dc491f8f283f3b01a4e61b0fd6
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-03-27 17:08:04 +0900

    CI: cloudbuild.yaml

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+variable "project" {}
+variable "region" {}
+
+terraform {
+  backend "gcs" {
+    bucket = "klaraworks-tfstate"
+    path   = "prototip-2.tfstate"
+    credentials = "../../../secrets/klaraworks-tfstate.json"
+  }
+}
+
+provider "google-beta" {
+  credentials = file("../../../secrets/prototip-2-terraformer.json")
+  project     = var.project
+  region      = var.region
+  version     = "~> 3.14.0"
+}
+
+resource "google_cloudbuild_trigger" "default" {
+  provider = google-beta
+  project = var.project
+  github {
+    owner = "kirisaki"
+    name = "prototip-2.kirisaki.dev"
+    push {
+      tag = "[0-9]+\\.[0-9]+\\.[0-9]"
+    }
+  }
+  filename = "cloudbuild.yml"
+}
+
