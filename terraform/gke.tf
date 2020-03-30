858271d256b27ab410ac330975d0d025df26192f
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-03-30 14:58:06 +0900

    it works!!

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -30,6 +30,7 @@ resource "google_container_node_pool" "primary_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
     ]
   }
 }
@@ -57,6 +58,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
     ]
   }
 }
